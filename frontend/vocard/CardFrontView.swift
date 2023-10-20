48a3d609d7aead043e9f275628fe45e33cfd8c35
windowcow <strongwindowcow@gmail.com>
2023-10-20 16:47:25 +0900

    Made Card Shape

--- frontend/vocard/CardFrontView.swift
+++ frontend/vocard/CardFrontView.swift
@@ -11,20 +11,21 @@ struct CardFrontView: View {
     @State var cardData: CardData
     
     var body: some View {
-        
-        Text("Hello, World!")
+        VStack {
+            
+        }
     }
 }
 
 #Preview {
-    let cardData: CardData = CardData(
-        originalWord: "사과",
-        translatedWord: "apple",
-        englishDefinition: "a round fruit with red, yellow, or green skin and firm white flesh",
-        exampleSentence: "She ate a fresh apple for breakfast.",
-        learningScore: 5,
-        consecutiveCorrectStreak: 3
+    CardFrontView(cardData:
+        CardData(
+            originalWord: "사과",
+            translatedWord: "apple",
+            englishDefinition: "a round fruit with red, yellow, or green skin and firm white flesh",
+            exampleSentence: "She ate a fresh apple for breakfast.",
+            learningScore: 5,
+            consecutiveCorrectStreak: 3
+        )
     )
-
-    CardFrontView(cardData: cardData)
 }
---
+++ frontend/vocard/CardShapeView.swift
@@ -0,0 +1,58 @@
+//
+//  CardShapeView.swift
+//  vocard
+//
+//  Created by windowcow on 10/20/23.
+//
+
+import SwiftUI
+
+struct CardShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let width = rect.size.width
+        let height = rect.size.height
+        path.move(to: CGPoint(x: 0, y: 0.13201*height))
+        path.addCurve(to: CGPoint(x: 0.05917*width, y: 0.09584*height), control1: CGPoint(x: 0, y: 0.11203*height), control2: CGPoint(x: 0.02649*width, y: 0.09584*height))
+        path.addLine(to: CGPoint(x: 0.94083*width, y: 0.09584*height))
+        path.addCurve(to: CGPoint(x: width, y: 0.13201*height), control1: CGPoint(x: 0.97351*width, y: 0.09584*height), control2: CGPoint(x: width, y: 0.11203*height))
+        path.addLine(to: CGPoint(x: width, y: 0.96383*height))
+        path.addCurve(to: CGPoint(x: 0.94083*width, y: height), control1: CGPoint(x: width, y: 0.98381*height), control2: CGPoint(x: 0.97351*width, y: height))
+        path.addLine(to: CGPoint(x: 0.05917*width, y: height))
+        path.addCurve(to: CGPoint(x: 0, y: 0.96383*height), control1: CGPoint(x: 0.02649*width, y: height), control2: CGPoint(x: 0, y: 0.98381*height))
+        path.addLine(to: CGPoint(x: 0, y: 0.13201*height))
+        path.closeSubpath()
+        path.move(to: CGPoint(x: 0, y: 0.03617*height))
+        path.addCurve(to: CGPoint(x: 0.05917*width, y: 0), control1: CGPoint(x: 0, y: 0.01619*height), control2: CGPoint(x: 0.02649*width, y: 0))
+        path.addLine(to: CGPoint(x: 0.41124*width, y: 0))
+        path.addCurve(to: CGPoint(x: 0.47041*width, y: 0.03617*height), control1: CGPoint(x: 0.44392*width, y: 0), control2: CGPoint(x: 0.47041*width, y: 0.01619*height))
+        path.addLine(to: CGPoint(x: 0.47041*width, y: 0.46474*height))
+        path.addCurve(to: CGPoint(x: 0.41124*width, y: 0.5009*height), control1: CGPoint(x: 0.47041*width, y: 0.48471*height), control2: CGPoint(x: 0.44392*width, y: 0.5009*height))
+        path.addLine(to: CGPoint(x: 0.05917*width, y: 0.5009*height))
+        path.addCurve(to: CGPoint(x: 0, y: 0.46474*height), control1: CGPoint(x: 0.02649*width, y: 0.5009*height), control2: CGPoint(x: 0, y: 0.48471*height))
+        path.addLine(to: CGPoint(x: 0, y: 0.03617*height))
+        path.closeSubpath()
+        path.move(to: CGPoint(x: 0.45858*width, y: 0.01447*height))
+        path.addLine(to: CGPoint(x: 0.56805*width, y: 0.08116*height))
+        path.addCurve(to: CGPoint(x: 0.63314*width, y: 0.09584*height), control1: CGPoint(x: 0.58876*width, y: 0.09584*height), control2: CGPoint(x: 0.62081*width, y: 0.09704*height))
+        path.addLine(to: CGPoint(x: 0.63314*width, y: 0.12839*height))
+        path.addLine(to: CGPoint(x: 0.45266*width, y: 0.12839*height))
+        path.addLine(to: CGPoint(x: 0.45858*width, y: 0.01447*height))
+        path.closeSubpath()
+        return path
+    }
+}
+
+struct CardShapeView: View {
+    var body: some View {
+        VStack {
+            CardShape()
+                .padding()
+        }
+        
+    }
+}
+
+#Preview {
+    CardShapeView()
+}
