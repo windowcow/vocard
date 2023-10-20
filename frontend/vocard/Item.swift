23fb622484cd8fca8f5f18c273892bc1248a106f
windowcow <strongwindowcow@gmail.com>
2023-10-20 11:21:52 +0900

    first

---
+++ frontend/vocard/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  vocard
+//
+//  Created by windowcow on 10/20/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
