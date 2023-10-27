07a969a5a4f5566ea9ec12f0dd1421729df0f2e2
windowcow <strongwindowcow@gmail.com>
2023-10-27 18:08:56 +0900

    카드 앞 뒤 완성. 이제 화면 전체 만들어야함

--- frontend/vocard/CardView/CardBackView.swift
+++ frontend/vocard/CardView/CardBackView.swift
@@ -8,11 +8,84 @@
 import SwiftUI
 
 struct CardBackView: View {
+    @State var cardData: CardData
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            VStack {
+                Spacer()
+
+                HStack {
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Text("SCORE  **\(cardData.learningScore)**")
+                        .font(.title3)
+                    Spacer()
+
+                }
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+                Spacer()
+
+                Text("\(cardData.originalWord)")
+                    .font(.largeTitle)
+                    .bold()
+                
+                
+                Text("\(cardData.englishDefinition)")
+                    .padding()
+                    .background(Color("CardBackInsideColor"), in: .rect(cornerRadius: 15))
+                    .padding()
+
+                Spacer()
+                VStack {
+                    Image("SampleImage")
+                        .resizable()
+                        .scaledToFill()
+                        .padding()
+                    Text("\(cardData.exampleSentence)")
+                        .padding()
+                }
+                
+                .background(Color("CardBackInsideColor"), in: .rect(cornerRadius: 15))
+                .padding()
+                Spacer()
+
+
+            }
+            
+        }
+        .foregroundStyle(.white)
+        .background(Color("CardBackColor") ,
+                    in: CardBackShape())
+
+        
     }
 }
 
 #Preview {
-    CardBackView()
-}
+    ZStack {
+        CardBackView(cardData:
+            CardData(
+                originalWord: "apple",
+                translatedWord: "사과",
+                englishDefinition: "a round fruit with red, yellow, or green skin and firm white flesh",
+                exampleSentence: "She ate a fresh apple for breakfast.",
+                learningScore: 5,
+                consecutiveCorrectStreak: 3
+            )
+        )
+        .padding()
+    }}
