9e7912b026362564096e27f2b9833f01cfcf9aa5
windowcow <strongwindowcow@gmail.com>
2023-12-07 20:28:12 +0900

    asdf

--- frontend/vocard/Pages/CardGPTPage/CardGPTPage.swift
+++ frontend/vocard/Pages/CardGPTPage/CardGPTPage.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct CardGPTPage: View {
     var headword: String
@@ -41,6 +42,9 @@ struct CardGPTPage: View {
                     .foregroundStyle(.black)
                 
                 /// 사용자 - gpt 평가가 여기 저장됨. ForEach로 구현 예정
+                Divider()
+                
+                CardGPTPage_Cells(meaning: meaning)
                 
                 
                 Spacer()
@@ -53,6 +57,47 @@ struct CardGPTPage: View {
     }
 }
 
+@Model class StudyMaterial {
+    var sourceMeaning: MeaningData
+    var myMessage: String = ""
+    var gptMessage: String = ""
+    var imageURL: URL?
+    
+    init(sourceMeaning: MeaningData, myMessage: String, gptMessage: String, imageURL: URL? = nil) {
+        self.sourceMeaning = sourceMeaning
+        self.myMessage = myMessage
+        self.gptMessage = gptMessage
+        self.imageURL = imageURL
+    }
+}
+
+struct CardGPTPage_Cells: View {
+    var meaning: MeaningData
+    
+    var body: some View {
+        LazyVStack {
+            
+        }
+    }
+}
+
+struct CardGPTPage_Cell: View {
+    
+    
+    var body: some View {
+        ZStack {
+            Color.clear
+            HStack {
+                VStack {
+                    
+                }
+                
+                ///AsyncImage
+            }
+        }
+    }
+}
+
 struct CardGPTPage_TextField: View {
     @State private var text: String = ""
     var body: some View {
@@ -62,6 +107,9 @@ struct CardGPTPage_TextField: View {
             }
             .frame(maxWidth: .infinity)
             Spacer()
+            
+            
+            
             Button{
                 
             } label: {
